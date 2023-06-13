c7f9086fc65f612b56815b954d6575ac9c9602ed
Paranoj <e.behterev@yandex.ru>
2023-06-13 22:31:57 +0700

    init

---
+++ src/test/java/com/shoponline/gatewayserver/ApiGatewayServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.shoponline.gatewayserver;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServerApplicationTests {
+
+	@Disabled
+	void contextLoads() {
+	}
+
+}
